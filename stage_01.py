0426c97c1b03b0be91d84233dbfa92a86f3c9eb8
ksachin5136 <ksachin5136@gmail.com>
2024-06-12 22:03:34 +0530

    dcv repro comit

---
+++ stage_01.py
@@ -0,0 +1,2 @@
+with open("artifact.txt", "w") as f:
+    f.write("my name is sachin and here i am executing stage_01 and more data")
---
+++ stage_02.py
@@ -0,0 +1,5 @@
+with open("artifact.txt", "r") as f:
+    text = f.read()
+print("i am from statge_02", text)
+
+print("i extra line 2 from statge_02.py")
