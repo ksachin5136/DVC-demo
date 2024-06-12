e8ac484467768e5c0fd47dc3fcff7332a17724fe
ksachin5136 <ksachin5136@gmail.com>
2024-06-12 15:55:19 +0530

    first commit dvc

---
+++ demo_01.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = [
+
+    {"name": "sunny", "age": 27, "city": "bhopal"},
+    {"name": "krish", "age": 35, "city": "bengaluru"},
+    {"name": "sudh", "age": 33, "city": "delhi"},
+    {"name": "vikas", "age": 29, "city": "up"}
+]
+
+df = pd.DataFrame(data)
+
+df.to_csv("data/data.csv", index=False)
